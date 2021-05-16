56b2d084f08217ff487307f26fa1c0075cc339de
boylee-17 <leeby1717@gmail.com>
2021-05-16 21:25:32 +0900

    :gitignore파일추가

---
+++ first.py
@@ -0,0 +1,18 @@
+# print(1+2)
+# print("멋쟁이 사자처럼")
+
+# number = 10
+# print(number+1)
+
+## 인풋 옆에 있는 친구가 프롬프트임
+
+# banjang = input("반장의 이름을 입력하세요 : ")
+# print("우리반 반장 이름은", banjang)
+
+number1 = int(input("첫번째 숫자를 입력하세요 : "))
+number2 = int(input("두번째 숫자를 입력하세요 : "))
+print(number1 + number2)
+
+##인풋함수는 문자열을 입력받는 함수일 뿐임
+##-> 문자+문자 는 그냥 두 문자를 나란히 붙히는 꼴임
+##-> 숫자의 합산을 원하면 int 추가해야함
---
+++ second.py
@@ -0,0 +1,14 @@
+
+##이건 정수형
+num1 = int(input("여기에 첫번째 정수를 입력하세요 :"))
+num2 = int(input("여기에 두번째 정수를 입력하세요 :"))
+
+##이건 실수형
+num1 = float(input("여기에 첫번째 정수를 입력하세요 :"))
+num2 = float(input("여기에 두번째 정수를 입력하세요 :"))
+
+# print(num1 / num2) #몫만 구하기
+# print(num1 % num2) #나머지만 구하기
+# print(num1 // num2) #몫과 나머지 구하기
+
+print(num1 ** num2) #거듭 제곱 구하기 뒤에 오는 애가 승
